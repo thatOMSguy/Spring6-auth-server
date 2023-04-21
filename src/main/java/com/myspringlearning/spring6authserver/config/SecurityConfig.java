c9953ed1f79a7d39e41960872a91943e807fac86
Ravi Kumar <84226388+thatOMSguy@users.noreply.github.com>
2023-04-21 18:13:37 +0530

    beautify

--- src/main/java/com/myspringlearning/spring6authserver/config/SecurityConfig.java
+++ src/main/java/com/myspringlearning/spring6authserver/config/SecurityConfig.java
@@ -46,15 +46,15 @@ public class SecurityConfig {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
         http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
                 .oidc(Customizer.withDefaults());    // Enable OpenID Connect 1.0
-        http
-                // Redirect to the login page when not authenticated from the
-                // authorization endpoint
-                .exceptionHandling((exceptions) -> exceptions
+        // Redirect to the login page when not authenticated from the
+        // authorization endpoint
+        http.exceptionHandling((exceptions) -> exceptions
                         .authenticationEntryPoint(
                                 new LoginUrlAuthenticationEntryPoint("/login"))
                 )
                 // Accept access tokens for User Info and/or Client Registration
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
 
         return http.build();
     }
